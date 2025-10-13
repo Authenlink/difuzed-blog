a593697bc1c2a6ac377a517515972fdac0e21005
Hugo <hugomisery1@gmail.com>
2025-10-13 21:03:33 +0200

    post update config

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -16,7 +16,7 @@ export default async function BlogPage() {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {posts.map((post) => (
-            <BlogCard key={post.id} post={post} />
+            <BlogCard key={post.id} article={post} />
           ))}
         </div>
       )}
